3ba81eb47ec597215beb17ce2d26c6faf16934fa
Ahmed Y. Azzam <109034951+drazzam@users.noreply.github.com>
2023-01-29 01:17:34 +0200

    Update DCI.py

--- DCI.py
+++ DCI.py
@@ -26,46 +26,44 @@ st.title("Delayed Cerebral Ischemia Prediction App")
 # Get user input for the 12 variables
 sex = st.selectbox("Select gender", ["Male", "Female"])
 sex = 0 if sex == "Male" else 1
-age = st.number_input("Enter age", min_value=0, max_value=120, value=50)
-bmi = st.number_input("Enter BMI", min_value=0, max_value=50, value=30)
-hh_score = st.number_input("Enter Hunt and Hess score", min_value=0, max_value=5, value=3)
-mfisher = st.number_input("Enter modified Fisher scale", min_value=0, max_value=5, value=3)
+age = st.number_input("Enter age: ")
+bmi = st.number_input("Enter BMI: ")
+hh_score = st.number_input("Enter Hunt and Hess score: ")
+mfisher = st.number_input("Enter modified Fisher scale: ")
 htn = st.selectbox("Hypertension", ["Yes", "No"])
 htn = 1 if htn == "Yes" else 0
 smoke = st.selectbox("Smoking", ["Yes", "No"])
 smoke = 1 if smoke == "Yes" else 0
-size_mm = st.number_input("Enter size of aneurysm in mm", min_value=0, max_value=100, value=25)
+size_mm = st.number_input("Enter size of aneurysm in mm: ")
 tx = st.selectbox("Treatment modality", ["Microsurgical Clipping", "Endovascular Coiling"])
 tx = 1 if tx == "Microsurgical Clipping" else 2
-mrs_discharge = st.number_input("Enter mrs at discharge", min_value=0, max_value=5, value=2)
+mrs_discharge = st.number_input("Enter mrs at discharge: ")
 infarct = st.selectbox("Cerebral infarction", ["Yes", "No"])
 infarct = 1 if infarct == "Yes" else 0
 monocytes = st.number_input("Enter monocyte count in 10^3/uL: ")
 
 # Create a dataframe from the user input
-user_input = pd.DataFrame(
-{'sex': sex,
-'age': age,
-'bmi': bmi,
-'hh_score': hh_score,
-'mfisher': mfisher,
-'htn': htn,
-'smoke': smoke,
-'size_mm': size_mm,
-'tx': tx,
-'mrs_discharge': mrs_discharge,
-'infarct': infarct,
-'monocytes': monocytes
-}, index=[0])
+if st.button("Predict"):
+ # Create a new data point using the user-entered values
+ user_input = pd.DataFrame(
+ {'sex': sex,
+ 'age': age,
+ 'bmi': bmi,
+ 'hh_score': hh_score,
+ 'mfisher': mfisher,
+ 'htn': htn,
+ 'smoke': smoke,
+ 'size_mm': size_mm,
+ 'tx': tx,
+ 'mrs_discharge': mrs_discharge,
+ 'infarct': infarct,
+ 'monocytes': monocytes
+ }, index=[0])
 
 # Use the model to make a prediction
 prediction = model.predict(user_input)
 
 #Show the prediction
 st.write("The Probability of Developing Delayed Cerebral Ischemia Is: ", (prediction[0]*100),"%")
-
-# Add a button to rerun the app
-if st.button("Predict again"):
- pass
 
 
